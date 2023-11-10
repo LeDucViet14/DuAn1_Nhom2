8d09386327ce06cd1daf191de938e3865af7af74
LeDucViet14 <ducvietle1404@gmail.com>
2023-11-10 20:58:06 +0700

    cat-giao-dien-php

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php
+    include "model/contact.php";
+    include "model/about.php";
+    include "model/pdo.php";
+    include "model/rooms.php";
+    include "global.php";
+    include "view/header.php";
+    $contact_home = loadContact();
+    if(isset($_GET['act']) && ($_GET['act'] != "")){
+        $act = $_GET['act'];
+        switch ($act) {
+            case 'home':
+                include "view/home.php";
+                break;
+            case 'about':
+                $about = load_about();
+                include "view/about.php";
+                break;
+            case 'contact':
+                include "view/contact.php";
+                break;
+            case 'facilities':
+                include "view/facilities.php";
+                break;
+            case 'rooms':
+                $rooms = loadall_room();
+                foreach($rooms as $room){
+                    $features = loadall_features_room($room['id']);
+                    $facilities = loadall_facilities_room($room['id']);
+                }
+                include "view/rooms.php";
+                break;
+            case 'room_details':
+                include 'view/rooms_details.php';
+                break;
+
+            default:
+            include "view/home.php";
+            break;
+        }
+    }else{
+        include "view/home.php";
+    }
+
+    include "view/footer.php";
+?>
---
+++ view/rooms_details.php
@@ -0,0 +1,140 @@
+
+    <main>
+
+        <div class="container">
+            <div class="row">
+
+                <div class="col-lg-12 mb-4 my-5 px-4">
+                    <h2 class="fw-bold">Name Room</h2>
+                    <div style="font-size: 14px;">
+                        <a href="index.html" class="text-secondary text-decoration-none">HOME</a>
+                        <span> > </span>
+                        <a href="rooms.html" class="text-secondary text-decoration-none">ROOMS</a>
+                    </div>
+                </div>
+
+
+
+                <div class="col-lg-7 col-md-12 px-4">
+                    <div id="carouselExample" class="carousel slide">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img src="images/rooms/IMG_11892.png" class="d-block w-100 rounded" alt="...">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="images/rooms/IMG_39782.png" class="d-block w-100 rounded" alt="...">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="images/rooms/IMG_42663.png" class="d-block w-100 rounded" alt="...">
+                            </div>
+                        </div>
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample"
+                            data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExample"
+                            data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button>
+                    </div>
+                </div>
+
+                <div class="col-lg-5 col-md-12 px-4">
+                    <div class="card mb-4 border-0 shadow-sm rounded-3">
+                        <div class="card-body">
+                            <h4>$50 per night</h4>
+
+                            <div class="raiting mb-3">
+                                <i class="fa-solid fa-star" style="color: rgb(241, 241, 11);"></i>
+                                <i class="fa-solid fa-star" style="color: rgb(241, 241, 11);"></i>
+                                <i class="fa-solid fa-star" style="color: rgb(241, 241, 11);"></i>
+                                <i class="fa-solid fa-star" style="color: rgb(241, 241, 11);"></i>
+                            </div>
+
+                            <div class="features mb-4">
+                                <h6 class="mb-1">Features</h6>
+                                <span class="badge rounded-pill text-bg-light text-dark text-wrap">
+                                    2 rooms
+                                </span>
+                                <span class="badge rounded-pill text-bg-light text-dark text-wrap">
+                                    1 Batroom
+                                </span>
+                                <span class="badge rounded-pill text-bg-light text-dark text-wrap">
+                                    2 Balcony
+                                </span>
+                                <span class="badge rounded-pill text-bg-light text-dark text-wrap">
+                                    2 sofa
+                                </span>
+                            </div>
+
+                            <div class="facilities mb-4">
+                                <h6 class="mb-1">Facilities</h6>
+                                <span class="badge rounded-pill text-bg-light text-dark text-wrap">
+                                    Wifi
+                                </span>
+                                <span class="badge rounded-pill text-bg-light text-dark text-wrap">
+                                    Television
+                                </span>
+                                <span class="badge rounded-pill text-bg-light text-dark text-wrap">
+                                    AC
+                                </span>
+                                <span class="badge rounded-pill text-bg-light text-dark text-wrap">
+                                    Room heater
+                                </span>
+                            </div>
+
+                            <div class="Guests mb-4">
+                                <h6 class="mb-1">Guests</h6>
+                                <span class="badge rounded-pill text-bg-light text-dark text-wrap">
+                                    4 Children
+                                </span>
+                                <span class="badge rounded-pill text-bg-light text-dark text-wrap">
+                                    5 Adults
+                                </span>
+                            </div>
+
+                            <div class="area mb-4">
+                                <h6 class="mb-1">Area</h6>
+                                <span class="badge rounded-pill text-bg-light text-dark text-wrap">
+                                    Ha Noi
+                                </span>
+                            </div>
+
+                            <a href="#" class="btn btn-sm text-white custom-bg shadow-none w-100 mb-1">Book now</a>
+
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-12 mt-4 px-4">
+                    <div class="mb-5">
+                        <h5>Description</h5>
+                        <p>Mô tả (đẹp, tiện nghi....)</p>
+                    </div>
+                    <div>
+                        <h5 style="margin-bottom:10px ;">Reviews & Ratings</h5>
+                        <div>
+                            <div class="d-flex align-items-center mb-2">
+                                <img src="" alt="ảnh">
+                                <h6 class="m-0 ms-2">Random user 1</h6>
+                            </div>
+                            <p>Le Duc Viet Le Duc Viet Le Duc Viet Le Duc Viet Le Duc Viet Le Duc Viet Le Duc Viet Le
+                                Duc
+                                Viet Le Duc Viet Le Duc Viet Le Duc Viet Le Duc Viet </p>
+                            <div class="raiting">
+                                <i class="fa-solid fa-star" style="color: rgb(241, 241, 11);"></i>
+                                <i class="fa-solid fa-star" style="color: rgb(241, 241, 11);"></i>
+                                <i class="fa-solid fa-star" style="color: rgb(241, 241, 11);"></i>
+                                <i class="fa-solid fa-star" style="color: rgb(241, 241, 11);"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+
+    </main>
