b97cac64d6e742d0773461e5add19774ca038554
Lahoanganh0708 <lahoanganh2004@gmail.com>
2023-11-23 23:04:10 +0700

    quản lí user

--- admin/header.php
+++ admin/header.php
@@ -1,37 +1,37 @@
 <div class="container-fluid bg-dark text-light p-3 d-flex sticky-top algin-items-center justify-content-between">
-     <h3 class="mb-0 h-font">LEO HOTEL</h3>
-     <a href="logout.php" class="btn btn-light btn-sm">LOG OUT</a>
- </div>
- <div class="container-fluid">
-     <div class="row">
-         <div class="col-lg-2 bg-dark border-top border-3 border-secondary" id="dashboard-menu">
-             <nav class="navbar navbar-expand-lg navbar-dark">
-                 <div class="container-fluid flex-lg-column align-items-stretch">
-                     <h4 class="mt-2 text-light">ADMIN PANEL</h4>
-                     <button class="navbar-toggler shadow-none" type="button" data-toggle="collapse" data-target="#adminDropdown" aria-controls="adminDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                         <span class="navbar-toggler-icon"></span>
-                     </button>
-                     <div class="collapse navbar-collapse flex-column align-items-stretch mt-2" id="adminDropdown">
-                         <ul class="nav nav-pills flex-column">
-                             <li class="nav-item">
-                                 <a class="nav-link text-white" href="#">Dashboard</a>
-                             </li>
-                             <li class="nav-item">
-                                 <a class="nav-link text-white" href="#">Rooms</a>
-                             </li>
-                             <li class="nav-item">
-                                 <a class="nav-link text-white" href="#">Users</a>
-                             </li>
-                             <li class="nav-item">
-                                 <a class="nav-link text-white" href="index.php?act=settings">Settings</a>
-                             </li>
-                         </ul>
-                     </div>
-                 </div>
-             </nav>
-         </div>
-     </div>
- </div>
- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <h3 class="mb-0 h-font">LEO HOTEL</h3>
+    <a href="logout.php" class="btn btn-light btn-sm">LOG OUT</a>
+</div>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-lg-2 bg-dark border-top border-3 border-secondary" id="dashboard-menu">
+            <nav class="navbar navbar-expand-lg navbar-dark">
+                <div class="container-fluid flex-lg-column align-items-stretch">
+                    <h4 class="mt-2 text-light">ADMIN PANEL</h4>
+                    <button class="navbar-toggler shadow-none" type="button" data-toggle="collapse" data-target="#adminDropdown" aria-controls="adminDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse flex-column align-items-stretch mt-2" id="adminDropdown">
+                        <ul class="nav nav-pills flex-column">
+                            <li class="nav-item">
+                                <a class="nav-link text-white" href="index.php?act=dashboard">Dashboard</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-white" href="index.php?act=rooms">Rooms</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-white" href="index.php?act=users">Users</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-white" href="index.php?act=settings">Settings</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </div>
+</div>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
--- admin/index.php
+++ admin/index.php
@@ -1,25 +1,70 @@
 <?php
-  session_start();
-  include '../model/pdo.php';
-  include '../model/login_admin.php';
-  include '../model/essentials.php';
-  adminLogin(); 
-  include './inc/links.php';
-  include './inc/scripts.php';
-  include 'header.php';
+session_start();
+include '../model/pdo.php';
+include '../model/login_admin.php';
+include '../model/essentials.php';
+adminLogin();
+include './inc/links.php';
+include './inc/scripts.php';
+include 'header.php';
+include '../admin/model/users.php';
 
-  if(isset($_GET['act'])){
-    $act = $_GET['act'];
-    switch ($act) {
-      case 'dashboard':
-        include 'dashboard.php';
-        break;
-      case 'settings':
-        include "settings.php";
-        break;
-      default:
-        # code...
-        break;
-    }
+if (isset($_GET['act'])) {
+  $act = $_GET['act'];
+  switch ($act) {
+    case 'dashboard':
+      include 'dashboard.php';
+      break;
+    case 'settings':
+      include "settings.php";
+      break;
+    case 'users':
+      // $sql = "SELECT * FROM user_cred";
+      // $rows = pdo_query($sql);
+      // $count = 1;  // Biến đếm
+      $list_user = loadall_user();
+      include "./users/users.php";
+      break;
+    case 'deleteusers':
+      if (isset($_GET['id']) && ($_GET['id'] > 0)) {
+        delete_user($_GET['id']);
+      }
+      $list_user = loadall_user();
+      include "./users/users.php";
+      break;
+    case 'suausers':
+      if (isset($_GET['id']) && ($_GET['id'] > 0)) {
+        $user = loadone_user($_GET['id']);
+      }
+      $list_user = loadall_user();
+      include "./users/update.php";
+      break;
+    case "updateusers":
+      if (isset($_POST['capnhat'])) {
+        $id = $_POST['id'];
+        $name = $_POST['name'];
+        $phone = $_POST['phonenum'];
+        $dob = $_POST['dob'];
+        $address = $_POST['address'];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        $img = $_FILES["profile"]["name"];
+        $target_dir = "./";
+        $target_file = $target_dir . basename($img);
+        $update_user = update_user($id, $name, $phone, $dob, $address, $email, $password, $target_file);
+        if (move_uploaded_file($_FILES["profile"]["tmp_name"], $target_file)) {
+          // Ảnh đã được tải lên thành công
+        } else {
+          // Lỗi khi tải ảnh lên
+        }
+      }
+      $list_user = loadall_user();
+      include "./users/users.php";
+      break;
+    case 'rooms':
+      include "rooms.php";
+      break;
+    default:
+      break;
   }
-?>
+}
